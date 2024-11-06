6d6ff815759a62898f683c9dc7791f01e7d9b84d
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-06 19:46:41 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -1,11 +1,12 @@
 public class Bill3 {
     public static void main(String[] args) {
-        String diner1 = args [0];
+        String diner1 = args [2];
         String diner2 = args [1];
-        String diner3 = args [2];
+        String diner3 = args [0];
         int bill = Integer.parseInt(args [3]);
         double splitedBill = Math.ceil (bill/3.0);
         System.out.println ("Dear " + diner1 + ", "  + diner2 + " and " + diner3 + ": pay " + splitedBill + " Shekels each.");
 
         }
     }
+
