63da00fc11e725eabc40204450274ffe4076725d
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-09 11:39:37 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,17 +1,21 @@
-// Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
-	public static void main(String[] args) {
-		// In Java, the command-line arguments args[0], args[1], ... are represented
-		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
-		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
-		// It then concatenates the resulting string with the rightmost hour-digit,
-		// and then uses parseInt to cast the resulting string as an int.
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
-	}
+    public static void main (String [] args) {
+
+        int hoursInput = Integer.parseInt (args [0]);
+        int convertedHours = (hoursInput - 12);
+        int minutesInput = Integer.parseInt (args [1]);
+
+
+
+        boolean aM = (hoursInput <= 11 && hoursInput >= 00);
+        boolean smallMinutes = minutesInput < 10;
+
+        if ((hoursInput > 23) || (hoursInput < 00) || (minutesInput > 59)|| (minutesInput < 0)) {
+            System.out.println ("illegal value");}
+        else if((aM) && (smallMinutes)) {System.out.println (hoursInput + ":" + "0" + minutesInput + " AM");}
+        else if((aM) && (!smallMinutes)) {System.out.println (hoursInput + ":" + minutesInput + " AM");}
+        else if((!aM) && (smallMinutes)) {System.out.println (hoursInput + ":" + "0" + minutesInput + " PM");}
+        else { System.out.println ( convertedHours + ":" + minutesInput + " PM");}
+
+    }
 }
